cadcfcaa4c8747b1ec1e96a9364d28f134b55fdc
skhiearth <skhiearth@gmail.com>
2021-07-06 02:46:54 +0530

    Dash App Init + Navbar

---
+++ Dashboard/sidebar.py
@@ -0,0 +1,47 @@
+# Import libraries
+from .server import app
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output
+import pandas as pd
+import plotly.express as px
+
+# Import styles
+from .style import *
+
+# ---COMPONENTS---
+IOTUBE_LOGO = "https://tube.iotex.io/static/media/logo_iotube.81e6aa74.svg"
+
+buttons = dbc.Row(
+    [
+        dbc.NavLink("Overview", href="/", active="exact", style=NAVLINK),
+        dbc.NavLink("Cross-chain Analytics", href="/crossChain", active="exact", style=NAVLINK),
+        dbc.NavLink("Cross-chain Token Transfer Analytics", href="/crossChainTokens", active="exact", style=NAVLINK),
+    ],
+    no_gutters=True,
+    className="ml-auto flex-nowrap mt-3 mt-md-0",
+    align="center",
+)
+
+navbar = dbc.Navbar([
+    html.A(
+        # Use row and col to control vertical alignment of logo / brand
+        dbc.Row(
+            [
+                dbc.Col(html.Img(src=IOTUBE_LOGO, height="30px")),
+                dbc.Col(dbc.NavbarBrand("Analytics", className="ml-2")),
+            ],
+            align="center",
+            no_gutters=True,
+        ), 
+        href="https://plotly.com",
+    ),
+    dbc.NavbarToggler(id="navbar-toggler", n_clicks=0),
+    dbc.Collapse(
+        buttons, id="navbar-collapse", navbar=True, is_open=False
+    ),
+], color="#3B3B3B", dark=True, style=NAVBAR_STYLE, sticky="top")
+
+content = html.Div(id="page-content", style=CONTENT_STYLE)
---
+++ Dashboard/style.py
@@ -0,0 +1,32 @@
+NAVBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "right": 0,
+    "background-color": "#262525",
+}
+
+CONTENT_STYLE = {
+    "margin-top": "75px",
+    "margin-left": "2rem",
+    "margin-right": "2rem",
+    "padding": "2rem 1rem",
+}
+
+NAVLINK = {
+    "color": "black",
+    "background-color": "#35FF99",
+    "margin-left": "8px",
+    "font-family": 'DM Mono, monospace',
+    "font-size": "14px"
+}
+
+COLUMN = {
+    "width": "45%",
+    "display": "inline-block",
+    "margin": "10px",
+}
+
+GRAPHS = {
+    "width": "100%",
+}
